1ebd75316687b45cc1b262a2e107b1ca358c00cf
DmitriySwan <dimonswan@gmail.com>
2018-09-25 15:13:12 +0300

    Add files via upload

---
+++ odd/js/javascript.js
@@ -0,0 +1,88 @@
+var btn = document.getElementById("NextButton");
+var c = 0;
+btn.addEventListener("click", hustle, false);
+
+function hustle() {
+    var firsts = document.getElementsByName("name1");
+    for (var i = 0; i < firsts.length; i++) {
+        if (firsts[0].checked) {
+            c++;
+            firsts[0].checked = false;
+            break;
+
+        }
+
+    }
+    var seconds = document.getElementsByName("name2");
+    for (var i = 0; i < seconds.length; i++) {
+        if (seconds[1].checked) {
+            c++;
+            seconds[1].checked = false;
+            break;
+        }
+    }
+    var thirds = document.getElementsByName("name3");
+    for (var i = 0; i < thirds.length; i++) {
+        if (thirds[2].checked) {
+            c++;
+            thirds[2].checked = false;
+            break;
+        }
+    }
+    var fourths = document.getElementsByName("name4");
+    for (var i = 0; i < fourths.length; i++) {
+        if (fourths[0].checked) {
+            c++;
+            fourths[0].checked = false;
+            break;
+        }
+    }
+    var fifths = document.getElementsByName("name5");
+    for (var i = 0; i < fifths.length; i++) {
+        if (fifths[1].checked) {
+            c++;
+            fifths[1].checked = false;
+            break;
+        }
+    }
+    var sixths = document.getElementsByName("name6");
+    for (var i = 0; i < sixths.length; i++) {
+        if (sixths[0].checked) {
+            c++;
+            sixths[0].checked = false;
+            break;
+        }
+    }
+    console.log(c);
+}
+var clickths = document.getElementsByName("name6");
+for (var i = 0; i < clickths.length; i++) {
+    clickths[i].onclick = hustla;
+}
+
+function hustla() {
+    var clas = document.getElementsByName("name6");
+    for (var i = 0; i < clas.length; i++) {
+        if (clas[0].checked) {
+            c++;
+            break;
+        }
+    }
+    popupslide();
+}
+
+function popupslide() {
+    if (c <= 3) {
+        var overlay1 = document.getElementById("overlay3");
+        overlay1.style.display = "block";
+    } else if (c == 4) {
+        var overlay2 = document.getElementById("overlay1");
+        overlay2.style.display = "block";
+    } else if (c == 5) {
+        var overlay3 = document.getElementById("overlay1");
+        overlay3.style.display = "block";
+    } else if (c == 6) {
+        var overlay4 = document.getElementById("overlay2");
+        overlay4.style.display = "block";
+    }
+}
