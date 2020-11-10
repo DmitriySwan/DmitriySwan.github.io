d1568c12b76c31f7ddadd3d0b8eccbf636332fc9
DmitriySwan <dimonswan@gmail.com>
2020-11-10 23:07:36 +0200

    Add files via upload

---
+++ lotus1/js/script.js
@@ -0,0 +1,13 @@
+
+$(function(){
+    
+    $(".btn_mnu").click(function() {
+        $(this).toggleClass("active");
+        $(".header_nav").toggleClass("active");
+        $("body").toggleClass("body");
+    });
+    
+    
+});
+
+
