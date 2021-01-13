beb6288da648b07997ad6a75b0b969ba23de7d8f
DmitriySwan <dimonswan@gmail.com>
2021-01-13 20:27:40 +0200

    Add files via upload

---
+++ profpol/js/script.js
@@ -0,0 +1,182 @@
+
+$(function(){
+    
+    $('ul.navmenu').flexMenu({
+        showOnHover: false,
+        linkText: "Еще...",
+        linkTitle: "Показать еще",
+        linkTextAll: "Меню",
+        linkTitleAll: "Развернуть меню", 
+        popupClass: 'more_dropdown'
+    });
+    
+    $('.intro_slider').slick({
+        slidesToShow: 1,
+        swipe: true,
+        infinity: true,
+        dots:true,
+        dotsClass: 'intro_dots',
+        arrows: true
+    });
+    
+    $('.intro_arrow1').on('click', function() {
+        $('.intro_slider').slick('slickPrev');
+    });
+    $('.intro_arrow2').on('click', function() {
+        $('.intro_slider').slick('slickNext');
+    });
+    
+    $('.trig_slider').slick({
+        slidesToShow: 8,
+        swipe: true,
+        infinity: true,
+        dots:true,
+        dotsClass: 'trig_dots',
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }
+        ]
+    });
+    
+    $('.reviews_slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        swipe: true,
+        infinity: true,
+        dots:true,
+        dotsClass: 'reviews_dots',
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    
+    $('.reviews_arrow1').on('click', function() {
+        $('.reviews_slider').slick('slickPrev');
+    });
+    $('.reviews_arrow2').on('click', function() {
+        $('.reviews_slider').slick('slickNext');
+    });
+    
+    $('.watched_slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        swipe: true,
+        infinity: true,
+        dots:true,
+        dotsClass: 'watched_dots',
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $('.watched_arrow1').on('click', function() {
+        $('.watched_slider').slick('slickPrev');
+    });
+    $('.watched_arrow2').on('click', function() {
+        $('.watched_slider').slick('slickNext');
+    });
+    
+    $('.tab_slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        swipe: true,
+        infinity: true,
+        dots:true,
+        dotsClass: 'tab_dots',
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $('.tab_arrow1').on('click', function() {
+        $('.tab_slider').slick('slickPrev');
+    });
+    $('.tab_arrow2').on('click', function() {
+        $('.tab_slider').slick('slickNext');
+    });
+    
+    $(".tab_item").not(":first").hide();
+    $(".wrapper .tab").click(function() {
+        $(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+        $(".tab_item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
+    
+    $.fn.extend({
+        toggleText: function(a, b){
+            return this.text(this.text() == b ? a : b);
+        }
+    });
+    
+    $(".about_text--showmore").hide();
+
+    $(".about_text--more").click(function() {
+        $(".about_text--showmore").slideToggle();
+        $(this).toggleText('Читать дальше', 'Свернуть');
+        $(this).toggleClass('active');
+    });
+    
+    $(".dropdown-menu").hide();
+
+    $(".dropdown-toggle").click(function() {
+        $(".dropdown-menu").slideToggle();
+        $(this).toggleClass('active');
+    });
+    
+    $(".header_tels--hidden").hide();
+
+    $(".header_tels").click(function() {
+        $(".header_tels--hidden").slideToggle();
+    });
+    
+
+});
+
+
