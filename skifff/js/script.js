b2e18e5b783c84a7cf64cc48341c4b8eaddbf37a
DmitriySwan <dimonswan@gmail.com>
2021-02-01 13:46:00 +0200

    Add files via upload

--- skifff/js/script.js
+++ skifff/js/script.js
@@ -15,7 +15,7 @@ $(function(){
         fade: true
     });
     
-    $(".intro_down").on("click", function(e) {
+    $(".down").on("click", function(e) {
         e.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top - 50;
@@ -36,12 +36,69 @@ $(function(){
             }
         ]
     });
+    
+    var $slider = $('.build_slider');
+
+    if ($slider.length) {
+        var currentSlide;
+        var slidesCount;
+        var sliderCounter = document.createElement('div');
+        sliderCounter.classList.add('build_counter');
+
+        var updateSliderCounter = function(slick, currentIndex) {
+            currentSlide = slick.slickCurrentSlide() + 1;
+            slidesCount = slick.slideCount;
+            $(sliderCounter).text(currentSlide + '/' +slidesCount)
+        };
+
+        $slider.on('init', function(event, slick) {
+            $slider.append(sliderCounter);
+            updateSliderCounter(slick);
+        });
+
+        $slider.on('afterChange', function(event, slick, currentSlide) {
+            updateSliderCounter(slick, currentSlide);
+        });
+
+        $slider.slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            speed: 1000,
+            fade: true
+        });
+    }
+    
+    $('.build_arrow1').on('click', function() {
+        $slider.slick('slickPrev');
+    });
+    
+    $('.build_arrow2').on('click', function() {
+        $slider.slick('slickNext');
+    });
 
     $('.spons_arrow1').on('click', function() {
         $('.spons_slider').slick('slickPrev');
     });
     $('.spons_arrow2').on('click', function() {
         $('.spons_slider').slick('slickNext');
+    });
+    
+    $('.also_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false
+    });
+
+    $('.also_arrow1').on('click', function() {
+        $('.also_slider').slick('slickPrev');
+    });
+    $('.also_arrow2').on('click', function() {
+        $('.also_slider').slick('slickNext');
     });
     
     jQuery(function($){
@@ -53,7 +110,13 @@ $(function(){
 
         $(".about_more").click(function() {
             $(".about_showmore").slideToggle();
-            $(this).toggleText('Читать дальше', 'Свернуть');
+            $(this).toggleClass('active');
+        });
+        
+        $(".card_p--more").hide();
+
+        $(".card_p--show").click(function() {
+            $(".card_p--more").slideToggle();
             $(this).toggleClass('active');
         });
     }
