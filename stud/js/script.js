39a06cc09f5ea4997a9ac30030b16c99806d84e6
DmitriySwan <dimonswan@gmail.com>
2021-03-05 20:38:05 +0200

    Add files via upload

---
+++ stud/js/script.js
@@ -0,0 +1,26 @@
+
+$(function(){
+    
+    $(".btn_mnu").click(function() {
+        $(this).toggleClass("active");
+        $(".header_nav ul").toggleClass("active");
+    });
+    
+    $(".top_item").slice(10).hide();
+
+    $(".top_more").click(function() {
+        $(".top_item").slice(10).fadeToggle();
+    });
+    
+    $(".reviews_item--more").hide();
+    
+    $('.reviews_item--arrow').click(function() {
+        $(this).closest('.reviews_item').find('.reviews_item--more').slideToggle();
+        $(this).toggleClass('active');
+    });
+    
+    $('.scroll').perfectScrollbar();
+
+});
+
+
